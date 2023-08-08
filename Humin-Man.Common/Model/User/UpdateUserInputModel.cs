c30d9254edb7b29872f53ab59d18371d0c9196e0
ejiro-wilkie <ejiro.wilkie@ozbul.com>
2023-08-08 15:28:57 +0300

    fix: change update user controller to use new UpdateUSerInputModel

---
+++ Humin-Man.Common/Model/User/UpdateUserInputModel.cs
@@ -0,0 +1,65 @@
+﻿namespace Humin_Man.Common.Model.User
+{
+    /// <summary>
+    /// Class that represent the update user input model.
+    /// </summary>
+    public class UpdateUserInputModel
+    {
+        /// <summary>
+        /// Gets or sets the first name.
+        /// </summary>
+        /// <value>
+        /// The first name.
+        /// </value>
+        public string FirstName { get; set; }
+        //hellos
+
+        /// <summary>
+        /// Gets or sets the last name.
+        /// </summary>
+        /// <value>
+        /// The last name.
+        /// </value>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email.
+        /// </summary>
+        /// <value>
+        /// The email.
+        /// </value>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the user.
+        /// </summary>
+        /// <value>
+        /// The name of the user.
+        /// </value>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the phone number.
+        /// </summary>
+        /// <value>
+        /// The phone number.
+        /// </value>
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
+        /// <value>
+        /// The password.
+        /// </value>
+        public string Password { get; set; }
+        /// <summary>
+        /// Gets or sets the id of the user.
+        /// </summary>
+        /// <value>
+        /// The id of the user.
+        /// </value>
+        public long Id { get; set; }
+
+    }
+}
